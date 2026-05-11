faf726183ba2a8ef38b83c4cc5d11d8b813d2e2b
novelhawk <novelhawk@gmail.com>
2026-05-11 21:52:22 +0200

    chore: update solid-js version

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,15 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import solidSvg from 'vite-plugin-solid-svg';
-// import devtools from 'solid-devtools/vite';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
+import solidSvg from "vite-plugin-solid-svg";
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-    solidSvg(),
-  ],
+  plugins: [devtools(), solidPlugin(), tailwindcss(), solidSvg()],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
